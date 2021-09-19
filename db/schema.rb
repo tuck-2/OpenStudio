fea1b21c9798b95e9b03bb7fa1d2da9750f5fa82
EC2 Default User <ec2-user@ip-172-31-46-84.ap-northeast-1.compute.internal>
2021-09-19 20:48:43 +0000

    [add]Audioモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_065505) do
+ActiveRecord::Schema.define(version: 2021_09_19_203440) do
+
+  create_table "audios", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
