4fc27cb2643c9cf077109e49e65850f9aa1376dd
EC2 Default User <ec2-user@ip-172-31-46-84.ap-northeast-1.compute.internal>
2021-09-21 09:20:36 +0000

    [add]マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources :users, only: [:show]
   root to: 'homes#top'
   resources :audios do
     resource :favorites, only: [:create, :destroy]
